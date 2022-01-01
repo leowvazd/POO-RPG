43b020a452a4dd590f589eb83f5fa4723b4f0c38
Leonardo Vaz <leonardovazlourenco@gmail.com>
2022-01-01 11:51:57 -0300

    up
    up

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using POO_RPG.src.Entities;
 
 namespace POO_RPG
 {
@@ -6,7 +7,16 @@ namespace POO_RPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hero arus = new Hero("Arus", 42, "Knight.");
+            Hero opponent = new Hero("Maleficus", 58, "Devil.");
+            Wizard jenica = new Wizard("Jenica", 33, "White Wizard.");
+
+            Console.WriteLine(arus);
+            Console.WriteLine(jenica);
+            Console.WriteLine(opponent);
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(jenica.Attack());
+            Console.WriteLine(jenica.Attack(5));
         }
     }
 }
